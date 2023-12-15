34b8b556416db23d281465709c445e92f0453133
shiga0119 <103249337+shiga0119@users.noreply.github.com>
2023-12-15 20:06:45 +0900

    Add files via upload

--- topview/src/twocam_node.cpp
+++ topview/src/twocam_node.cpp
@@ -23,6 +23,8 @@ private:
   Mat behind_low;
   int front_y;
   int behind_y;
+  int front_y_low=0;
+  int behind_y_low=0;
   void publishfront();
   void publishbehind();
   void fronthighcallback(sensor_msgs::msg::Image::SharedPtr msg);
@@ -44,10 +46,10 @@ ImagePublisher::ImagePublisher() : Node("twocam_node"){
   behind_low=cv::Mat(HEIGHT,WIDTH,CV_8UC3);
   behind_high=cv::Mat(HEIGHT,WIDTH,CV_8UC3);
   behind_low=cv::Mat(HEIGHT,WIDTH,CV_8UC3);
-  declare_parameter("front_high","/video0/image_raw");
-  declare_parameter("front_low","/video2/image_raw");
-  declare_parameter("behind_high","/video4/image_raw");
-  declare_parameter("behind_low","/video6/image_raw");
+  declare_parameter("front_high","/video2/image_raw");
+  declare_parameter("front_low","/video4/image_raw");
+  declare_parameter("behind_high","/video6/image_raw");
+  declare_parameter("behind_low","/video8/image_raw");
   declare_parameter("front_y",WIDTH);
   declare_parameter("behind_y",WIDTH);
   std::string fronthighpath_=get_parameter("front_high").as_string();
@@ -56,6 +58,14 @@ ImagePublisher::ImagePublisher() : Node("twocam_node"){
   std::string behindlowpath_=get_parameter("behind_low").as_string();
   front_y=get_parameter("front_y").as_int();
   behind_y=get_parameter("behind_y").as_int();
+  if(front_y>WIDTH){
+    front_y_low=front_y-WIDTH;
+    front_y=WIDTH;
+  }
+  if(behind_y>WIDTH){
+    behind_y_low=behind_y-WIDTH;
+    behind_y=WIDTH;
+  }
   fronthighsubscriber_=this->create_subscription<sensor_msgs::msg::Image>(fronthighpath_, 10,std::bind(&ImagePublisher::fronthighcallback,this,std::placeholders::_1));
   frontlowsubscriber_=this->create_subscription<sensor_msgs::msg::Image>(frontlowpath_, 10,std::bind(&ImagePublisher::frontlowcallback,this,std::placeholders::_1));
   behindhighsubscriber_=this->create_subscription<sensor_msgs::msg::Image>(behindhighpath_, 10,std::bind(&ImagePublisher::behindhighcallback,this,std::placeholders::_1));
@@ -100,22 +110,23 @@ void ImagePublisher::behindlowcallback(sensor_msgs::msg::Image::SharedPtr msg){
 }
 
 void ImagePublisher::publishfront(){
-  Mat cpyfront_high;
-  Mat cpyfront_low;
-  rotate(front_high,cpyfront_high,cv::ROTATE_90_COUNTERCLOCKWISE);
-  rotate(front_low,cpyfront_low,cv::ROTATE_180);
-  cpyfront_high=cpyfront_high(Rect(0,0,HEIGHT,front_y)).clone();
   Mat front=cv::Mat(HEIGHT+int(WIDTH*WIDTH/HEIGHT),WIDTH,CV_8UC3);
   front.setTo(Scalar(0));
-  Point2f fronthp0(0,0);
-  Point2f fronthp1(WIDTH,int(front_y*WIDTH/HEIGHT));
-  Point2f frontlp0(0,int(front_y*WIDTH/HEIGHT));
-  Point2f frontlp1(WIDTH,HEIGHT+int(front_y*WIDTH/HEIGHT));
-  if(front_high.cols>0&&front_high.rows>0){  
-  PinP_point(front,cpyfront_high,fronthp0,fronthp1);
+  if(front_high.cols>0&&front_high.rows>0){
+    Mat cpyfront_high; 
+    rotate(front_high,cpyfront_high,cv::ROTATE_90_COUNTERCLOCKWISE);
+    cpyfront_high=cpyfront_high(Rect(0,0,HEIGHT,front_y)).clone();
+     Point2f fronthp0(0,front_y_low);
+    Point2f fronthp1(WIDTH,int(front_y*WIDTH/HEIGHT)+front_y_low);
+    PinP_point(front,cpyfront_high,fronthp0,fronthp1);
   }
   if(front_low.cols>0&&front_low.rows>0){
-  PinP_point(front,cpyfront_low,frontlp0,frontlp1);
+    Mat cpyfront_low;
+    rotate(front_low,cpyfront_low,cv::ROTATE_180);
+    cpyfront_low=cpyfront_low(Rect(0,front_y_low,WIDTH,HEIGHT-front_y_low)).clone();
+    Point2f frontlp0(0,int(front_y*WIDTH/HEIGHT)+front_y_low);
+    Point2f frontlp1(WIDTH,HEIGHT+int(front_y*WIDTH/HEIGHT));
+    PinP_point(front,cpyfront_low,frontlp0,frontlp1);
   }
   cv_bridge::CvImage cv_image;
   sensor_msgs::msg::Image pub_image;
@@ -126,20 +137,22 @@ void ImagePublisher::publishfront(){
   waitKey(1);
 }
 void ImagePublisher::publishbehind(){
-  Mat cpybehind_high;
-  rotate(behind_high,cpybehind_high,cv::ROTATE_90_COUNTERCLOCKWISE);
-  cpybehind_high=cpybehind_high(Rect(0,0,HEIGHT,behind_y)).clone();
   Mat behind=cv::Mat(HEIGHT+int(WIDTH*WIDTH/HEIGHT),WIDTH,CV_8UC3);
   behind.setTo(Scalar(0));
-  Point2f behindhp0(0,0);
-  Point2f behindhp1(WIDTH,int(behind_y*WIDTH/HEIGHT));
-  Point2f behindlp0(0,int(behind_y*WIDTH/HEIGHT));
-  Point2f behindlp1(WIDTH,int(behind_y*WIDTH/HEIGHT)+HEIGHT);
-  if(behind_high.cols>0&&behind_high.rows>0){  
-  PinP_point(behind,cpybehind_high,behindhp0,behindhp1);
+  if(behind_high.cols>0&&behind_high.rows>0){
+    Mat cpybehind_high; 
+    rotate(behind_high,cpybehind_high,cv::ROTATE_90_COUNTERCLOCKWISE);
+    cpybehind_high=cpybehind_high(Rect(0,0,HEIGHT,behind_y)).clone(); 
+    Point2f behindhp0(0,behind_y_low);
+    Point2f behindhp1(WIDTH,int(behind_y*WIDTH/HEIGHT)+behind_y_low);
+    PinP_point(behind,cpybehind_high,behindhp0,behindhp1);
   }
   if(front_low.cols>0&&front_low.rows>0){
-  PinP_point(behind,behind_low,behindlp0,behindlp1);
+  Mat cpybehind_low;
+  cpybehind_low=behind_low(Rect(0,behind_y_low,WIDTH,HEIGHT-behind_y_low)).clone();
+  Point2f behindlp0(0,int(behind_y*WIDTH/HEIGHT)+behind_y_low);
+  Point2f behindlp1(WIDTH,int(behind_y*WIDTH/HEIGHT)+HEIGHT);
+  PinP_point(behind,cpybehind_low,behindlp0,behindlp1);
   }
   cv_bridge::CvImage cv_image;
   sensor_msgs::msg::Image pub_image;
